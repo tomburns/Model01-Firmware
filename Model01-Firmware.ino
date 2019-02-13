18c4c0b9ca2eef2fa8e585174368550d3156464d
Tom Burns <tomburns@gmail.com>
2019-02-12 19:33:25 -0600

    add braces

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -257,10 +257,10 @@ KEYMAPS(
    ___),
 
   [FUNCTION] =  KEYMAP_STACKED
-  (___,      Key_F1,           Key_F2,      Key_F3,     Key_F4,        Key_F5,           Key_CapsLock,
-   Key_Tab,  ___,              Key_mouseUp, ___,        Key_mouseBtnR, Key_mouseWarpEnd, Key_mouseWarpNE,
-   Key_Home, Key_mouseL,       Key_mouseDn, Key_mouseR, Key_mouseBtnL, Key_mouseWarpNW,
-   Key_End,  Key_PrintScreen,  Key_Insert,  ___,        Key_mouseBtnM, Key_mouseWarpSW,  Key_mouseWarpSE,
+  (___,      Key_F1,           Key_F2,      Key_F3,               Key_F4,                Key_F5,           Key_CapsLock,
+   Key_Tab,  ___,              Key_mouseUp, Key_LeftCurlyBracket, Key_RightCurlyBracket, Key_mouseWarpEnd, Key_mouseWarpNE,
+   Key_Home, Key_mouseL,       Key_mouseDn, Key_LeftParen,        Key_RightParen,        Key_mouseWarpNW,
+   Key_End,  Key_PrintScreen,  Key_Insert,  Key_LeftBracket,      Key_RightBracket,      Key_mouseWarpSW,  Key_mouseWarpSE,
    ___, Key_Delete, ___, ___,
    ___,
 
